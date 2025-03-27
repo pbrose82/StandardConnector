a4d6ea9140a867b75d09a253da41a2626b99eb39
pbrose82 <34488965+pbrose82@users.noreply.github.com>
2025-03-27 12:32:17 -0400

    Create error.middleware.ts

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import { logger } from '../utils/logger';
+
+export const errorMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  logger.error('Uncaught error:', err);
+  
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  
+  res.status(statusCode).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+  });
+};
